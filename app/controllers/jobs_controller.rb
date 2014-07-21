0421e760e8c9d4aa6a4828e4ebc5ec72138177fe
federicomaffei <federico.maffei@gmail.com>
2014-07-21 12:52:14 +0100

    added job creation controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,15 @@
 class JobsController < ApplicationController
+
 	def index
 		@jobs = Job.all
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.create(params[:job].permit(:name, :category, :company, :full_time, :detail, :location, :pay))
+		redirect_to '/jobs'
 	end
 end
--- spec/features/jobs_feature_spec.rb
+++ spec/features/jobs_feature_spec.rb
@@ -5,9 +5,29 @@ feature 'jobs index page' do
 		visit '/jobs'
 		expect(page).to have_content 'No jobs inserted yet.'
 	end
+
 	scenario 'a job is available.' do
-		job = Job.create name: 'Test job name', category: 'Test category', company: 'Test company', full_time: 'true', detail: 'Detailed description', location: 'EC2', pay: 20
+		job = Job.create name: 'Test job name', category: 'Bar', company: 'Test company', full_time: 'true', detail: 'Detailed description', location: 'EC2', pay: 10
 		visit '/jobs'
-		expect(page).to have_content 'Test job name Test category Test company EC2 20' 
+		expect(page).to have_content 'Test job name Bar Test company Detailed description EC2 10£' 
 	end
 end
+
+feature 'posting jobs' do
+	scenario 'a job is posted' do
+		visit '/jobs/new'
+		fill_in 'Name', with: 'Test job name'
+		select 'Bar', from: 'Category'
+		fill_in 'Company', with: 'Test Company'
+		select 'Full Time', from: 'Full time'
+		fill_in 'Detail', with: 'Detailed description'
+		fill_in 'Location', with: 'EC2'
+		fill_in 'Pay', with: 10
+		click_button 'Post a job'
+		expect(current_path).to eq '/jobs'
+		expect(page).to have_content 'Test job name Bar Test Company Detailed description EC2 10£'
+	end
+end
+
+
+
