0d45f899c18b28806ae75e412bd143052f9d4a6c
jwhyte88 <jwhyte@opentable.com>
2014-07-22 18:51:48 +0100

    Updated tests, added employer delete and update functionality

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -1,8 +1,18 @@
 class AdvertsController < ApplicationController
 
-def index
-	@employer = current_employer
-	@adverts = @employer.jobs
-end
+	def index
+		@employer = current_employer
+		@adverts = @employer.jobs
+	end
+
+	def edit
+		@job = Job.find params[:id]
+	end
+
+	def destroy
+		job = Job.find params[:id]
+		job.destroy
+		redirect_to '/'
+	end
 
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,9 +16,22 @@ class JobsController < ApplicationController
 
 	def create
 		authenticate_employer!
-		@job = Job.new(params[:job].permit(:advert_title, :category, :company, :full_time, :detail, :address, :wage, :email, :phone))
+		@job = Job.new(job_params)
 		@job.employer = current_employer
 		@job.save!
 		redirect_to '/jobs'
+	end
+
+	def update
+		authenticate_employer!
+		job = Job.find params[:id]
+		job.update(job_params)
+		redirect_to '/'
+	end
+
+	private
+
+	def job_params
+		params[:job].permit(:advert_title, :category, :company, :full_time, :detail, :address, :wage, :email, :phone)
 	end
 end
