925f3a96c25c8fe8602dcc21bce2987514484677
jwhyte88 <jwhyte@opentable.com>
2014-07-22 10:33:46 +0100

    merged map and employer versions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Job.create name: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', location: 'EC2A 3JX', pay: 10, email: 'queen@hoxton-test.com', phone: '12345678'
+Job.create name: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', location: 'SW6 1RE', pay: 10, email: 'queen@hoxton-test.com', phone: '12345678'
+
+Job.create name: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', location: 'EC2A 2DT', pay: 10, email: 'queen@hoxton-test.com', phone: '12345678'
