87103a756ef729188d57d1161c77cba06cea9e07
Robin <robindoble@gmail.com>
2014-07-23 11:30:35 +0100

    added links to sign in page for posting a job

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -9,21 +9,27 @@ describe 'visiting the landing page' do
 			expect(page).to have_content 'Find a job'
 		end
 
-	# context 'as a employer looking to post jobs' do 
+	context 'as a employer looking to post a job' do 
 
-	# 	it 'and clicking the post a job link' do 
-	# 		visit '/landing_page'
-	# 		expect(page).to have_context  
+		it 'and clicking the post a job link' do 
+			visit '/landing_page'
+			click_link 'Post a job'
+			expect(current_path).to eq '/employers/sign_in'
+			expect(page).to have_content 'Sign in'  
+		end
 
-	# 	end
+	end
 
-	# context 'as a job seeker looking to post jobs' do 
+	context 'as a job seeker looking to find a job' do 
 
-	# 	it 'and clicking the find a job link' do 
-	# 		visit '/landing_page'
-	# 		expect(page).to have_context 
+		it 'and clicking the find a job link' do 
+			visit '/landing_page'
+			click_link 'Find a job'
+			# expect(page).to have_content 'Find a job in London'
+			# expect(current_path).to eq '/jobs/index'
+		end
 
-	# 	end
+	end
 
 
 end
