9ad156b7d1604701d6a446d7d462dffd4c425d6b
James B Kemp <james@bibble.com>
2014-07-23 11:50:03 +0100

    testing orders content page

--- spec/features/orders_feature_spec.rb
+++ spec/features/orders_feature_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 describe 'orders page' do
 	let(:admin) {Admin.create(email: 'tester@testicle.com', password: 'teste', password_confirmation: 'teste')}
 
+	context 'not logged in as admin' do
+		it 'prompts you to sign in' do
+			visit '/orders'
+			expect(page).to have_content 'sign up'
+		end
+	end
+
 	context 'logged in as admin' do
 		
 		before do
@@ -15,13 +22,11 @@ describe 'orders page' do
 				expect(page).to have_content 'No orders yet'
 			end
 		end
-	end
 
-	context 'not logged in as admin' do
-		it 'prompts you to sign in' do
-			visit '/orders'
-			expect(page).to have_content 'sign up'
-		end
+
+
+
+
 	end
 
 end
