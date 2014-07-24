f4f7dc8105d7e360b60865590bca4f06e86f9627
Sasha Cooper <jinksy@gmail.com>
2014-07-24 19:42:46 +0100

    updated unchecking box test to match new algorithm

--- spec/features/map_feature_spec.rb
+++ spec/features/map_feature_spec.rb
@@ -12,17 +12,14 @@ describe 'primary map' do
 		it 'displays a marker for a job', js: true do
 			job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
 			visit '/jobs'
-			sleep 1
 			expect(page.evaluate_script("window.map.markers.length")).to eq 1
 		end
 
-		it 'stops displaying bar markers when you uncheck the bar box', js: true do
+		it 'stops including bar markers when you uncheck the bar box', js: true do
 			job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
 			visit '/jobs'
-			uncheck 'bar_box'
 			click_button 'Refine Search'
-			sleep 0.2
-			expect(page.evaluate_script("window.map.markers[0].getVisible()")).to be false
+			expect(page.evaluate_script("window.map.markers[0]")).to be nil
 		end
 
 	end
