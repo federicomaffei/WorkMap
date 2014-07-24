1ca2ac37c3587e8586fcb08439e2d406df5537e5
Robin <robindoble@gmail.com>
2014-07-24 15:19:22 +0100

    merging with master

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-	$('#filter_form').on('submit'), function(event){
+	$('.filter_form').on('submit', function(event){
 		event.preventDefault();
 		$.get('/jobs.json', $(this).serialize(), function(response){
 			console.log(response);
-		})
+		});
 	});
 })
