400755439f024474c9d1f7565996f604e4c6f6f0
Sasha Cooper <jinksy@gmail.com>
2014-07-24 16:53:20 +0100

    poltergeist tests added (failing on latest version)

--- spec/features/map_feature_spec.rb
+++ spec/features/map_feature_spec.rb
@@ -7,24 +7,25 @@ describe 'primary map' do
 		expect(page).to have_map
 	end
 
-	it 'displays a marker for a job', js: true do
-		job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
-		visit '/jobs'
-		sleep 1
-		expect(page.evaluate_script("window.map.markers.length")).to eq 1
+	context 'markers' do
+
+		it 'displays a marker for a job', js: true do
+			job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
+			visit '/jobs'
+			sleep 1
+			expect(page.evaluate_script("window.map.markers.length")).to eq 1
+		end
+
+		it 'stops displaying bar markers when you uncheck the bar box', js: true do
+			job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
+			visit '/jobs'
+			uncheck 'category_Bar'
+			sleep 1
+			expect(page.evaluate_script("window.map.markers[0].getVisible()")).to be false
+		end
+
 	end
 
-	# it 'displays a marker at two addresses given by an employer', js: true do
-	# 	job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
-	# 	visit '/jobs'
-	# 	sleep 1
-	# 	expect(page.evaluate_script("window.map.markers.length")).to eq 1
-	# end
 
-	context 'jobs by locations' do
-
-		xit 
-			job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
-		end		
 
 end
