bc10e250db808b86a4c983104025bb764cff3122
federicomaffei <federico.maffei@gmail.com>
2014-07-25 14:15:47 +0100

    fixed landing page spec errors

--- spec/features/map_feature_spec.rb
+++ spec/features/map_feature_spec.rb
@@ -18,8 +18,9 @@ describe 'primary map' do
 		it 'stops including bar markers when you uncheck the bar box', js: true do
 			job = Job.create advert_title: 'Waitress', category: 'Bar', company: 'Queen of Hoxton', full_time: 'false', detail: 'Detailed description', address: 'EC2A 3JX', wage: 10, email: 'queen@hoxton-test.com', phone: '12345678'
 			visit '/jobs'
+			uncheck 'bar_box'
 			click_button 'Refine Search'
-			expect(page.evaluate_script("window.map.markers[0]")).to be nil
+			expect(page.evaluate_script("window.map.markers[0].getVisible()")).to be false
 		end
 
 	end
