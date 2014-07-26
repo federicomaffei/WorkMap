7774f28866a811d1412c0bb6b46510769c581af6
federicomaffei <federico.maffei@gmail.com>
2014-07-26 12:46:02 +0100

    styled new icons and changed map color scheme

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -19,58 +19,136 @@ $(document).ready(function(){
 		streetViewControl: false,
 
 		styles: [
-		{
-			stylers: [
-			{ hue: "#00ff6f" },
-			{ saturation: -50 }
-			]
-		}, {
-			featureType: "road",
-			elementType: "geometry",
-			stylers: [
-			{ lightness: 100 },
-			{ visibility: "simplified" }
-			]
-		}, {
-			featureType: "transit",
-			elementType: "geometry",
-			stylers: [
-			{ hue: "#ff6600" },
-			{ saturation: +80 }
-			]
-		}, {
-			featureType: "transit",
-			elementType: "labels",
-			stylers: [
-			{ hue: "#ff0066" },
-			{ saturation: +80 }
-			]
-		}, {
-			featureType: "poi",
-			elementType: "labels",
-			stylers: [
-			{ visibility: "off" }
-			]
-		}, {
-			featureType: "poi.park",
-			elementType: "labels",
-			stylers: [
-			{ visibility: "on" }
-			]
-		}, {
-			featureType: "water",
-			elementType: "geometry",
-			stylers: [
-			{ hue: "#c4f4f4" }
-			]
-		}, {
-			featureType: "road",
-			elementType: "labels",
-			stylers: [
-			{ visibility: "off" }
-			]
-		}
-		]
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "saturation": 43
+            },
+            {
+                "lightness": -11
+            },
+            {
+                "hue": "#0088ff"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "hue": "#ff0000"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 99
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#808080"
+            },
+            {
+                "lightness": 54
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ece2d9"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ccdca1"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#767676"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#b8cb93"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.medical",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }
+]
 	});
 
 
