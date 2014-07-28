22057f43998b34c2cd6637f9ff999a705beff084
Robin <robindoble@gmail.com>
2014-07-28 19:53:47 +0100

    completed maps clustering

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,55 +1,37 @@
 $(document).ready(function(){
 	// window.map works like var, but changes scope of following object to whole window, so you can access it from tests
+	var markers = [];
+	var latitude = (localStorage.getItem('lat')) ? localStorage.getItem('lat') : 51.523126;
+	var longitude = (localStorage.getItem('lng')) ? localStorage.getItem('lng') : -0.087019;
 
-	// var latitude = (localStorage.getItem('lat')) ? localStorage.getItem('lat') : 51.523126;
-	// var longitude = (localStorage.getItem('lng')) ? localStorage.getItem('lng') : -0.087019;
-	// var currentMapCenter = (latitude,longitude);
-	// console.log(currentMapCenter);
+	console.log(latitude);
+	console.log(longitude);
 
-	var latitude = 51.523126;
-	var longitude = -0.087019;
-	
+	var mapOptions = {
+	  center: new google.maps.LatLng(latitude, longitude),
+	  zoom: 10,
+	 //  zoomControl: true,
+		// zoomControlOpt: {
+		// 	style: 'MEDIUM',
+		// 	position: 'CENTER_LEFT'
+		// },
+		// panControl: false,
+		// mapTypeControl: false,
+		// streetViewControl: false,
+		// styles: 
+		// [
+		// { "featureType": "landscape", 
+		// 	"elementType": "all", 
+		// 	"stylers": [
+		// 	{ "hue": "#ffffff" },
+		// 	{ "saturation": -100},
+		// 	{ "lightness": 100}
+  //     ]
+  //     },
+  //   ]
+	};
 
-	window.map = new GMaps({
-		div: '#map',
-		lat: latitude,
-		lng: longitude,	
-		zoom: 9,	
-		zoomControl: true,
-		zoomControlOpt: {
-			style: 'MEDIUM',
-			position: 'CENTER_LEFT'
-		},
-		panControl: false,
-		mapTypeControl: false,
-		streetViewControl: false,
-
-		
-		styles: 
-
-		[
-		{
-			"featureType": "landscape",
-			"elementType": "all",
-			"stylers": [
-			{
-				"hue": "#ffffff"
-			},
-			{
-				"saturation": -100
-			},
-			{
-				"lightness": 100
-			},
-			{
-                // "visibility": "simplified"
-            }
-            ]
-        },
-        ]
-
-    });
-
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 	var defaultBounds = new google.maps.LatLngBounds(
 		new google.maps.LatLng(51.2901, -0.5651),
@@ -69,83 +51,51 @@ $(document).ready(function(){
 	map.setOptions(zoom_options);
 
 	// creating a marketcluster object
-	// var mcOptions = {gridSize: 50, maxZoom: 15};
-	// var mc = new MarkerClusterer(map);
-	// var mc = new MarkerClusterer(map, [], mcOptions);
+	// var markerCluster = new MarkerClusterer(map);
+	var mcOptions = {gridSize: 50, maxZoom: 15};
 	// var mc = new MarkerClusterer(map, markers, mcOptions);
-
-
-	var renderMarker = function(job){
-		category = job.category;
-		var popup_template = $('#pop_up_job_advert').html();
-		var job_Info = Mustache.render(popup_template,job);	
-		var marker = map.addMarker({
-			lat: job.latitude,
-			lng: job.longitude,
-			title: job.advert_title,
-			category: job.category,
-			icon: "https://dl.dropboxusercontent.com/u/9315601/" + category + ".png",
-			infoWindow: { content: job_Info }
-		});
-	};
 
 	var calcDistanceKms = function(map,job) {
 		var mapCenter = map.getCenter();
 		var markerLocation = new google.maps.LatLng(job.latitude,job.longitude);
 	  var markerDistance = google.maps.geometry.spherical.computeDistanceBetween(mapCenter, markerLocation);
-	  // console.log(markerDistance/1000);
 	  return markerDistance/1000;
 	};
 
+	var clearMarkers = function() {
+		for (var i = 0; i < markers.length; i++) {
+	    markers[i].setMap(null);
+	  }
+	  markers = [];
+	};
+
+	var renderMarker = function(job){
+		category = job.category;
+		var popup_template = $('#pop_up_job_advert').html();
+		var job_Info = Mustache.render(popup_template,job);	
+		
+		var latLng = new google.maps.LatLng(job.latitude,job.longitude);
+		var marker = new google.maps.Marker({
+			position: latLng,
+			title: job.advert_title,
+			category: job.category,
+			icon: "https://dl.dropboxusercontent.com/u/9315601/" + category + ".png",
+			infoWindow: { content: job_Info }
+		});
+		markers.push(marker);
+	};
+
 	var submitFilterForm = function(jobs){
-		map.removeMarkers();
+		clearMarkers();
 		var advertsRefined = [];
 			jobs.forEach(function(job) {
-						// console.log('hello');
-						// console.log(job.max_distance);
 				if (calcDistanceKms(map,job) <= job.max_distance) {			
-						// console.log('hello');
 						renderMarker(job);
 						advertsRefined.push(job);
 					};
 			});
 		updateAdvertColumn(advertsRefined);
 	};
-
-	$.get("/jobs.json", function(jobs) {
-		var markers = [];
-		jobs.forEach(function(job) {
-			renderMarker(job);
-			// markers.push(job);
-		});
-		// var mc = new MarkerClusterer(map, markers, mcOptions);
-	});
-
-	$('#filter_form').submit(function(event){
-		event.preventDefault();
-			$.get('/jobs.json', $(this).serialize(), function(jobs){
-				submitFilterForm(jobs);
-			});
-	});
-
-
-	$('#search_box').on('submit', function(event) {
-			event.preventDefault();
-
-			GMaps.geocode({
-				address: $('#searchTextField').val(),
-				callback: function(results, status) {
-					if (status == 'OK') {
-						var latlng = results[0].geometry.location;
-						map.setCenter(latlng.lat(), latlng.lng());
-						$.get('/jobs.json',$('#filter_form').serialize(), function(jobs) {
-							submitFilterForm(jobs);
-					});
-				}
-			}
-		});
-	});
-
 
 	var updateAdvertColumn = function(jobs){
 		$('.advert_column').empty();
@@ -156,7 +106,38 @@ $(document).ready(function(){
 		});
 	};
 
+	$.get("/jobs.json", function(jobs) {
+		jobs.forEach(function(job) {
+			renderMarker(job);
+		});
+		var markerCluster = new MarkerClusterer(map, markers);
+		map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('legend'));
+	});
 
+	$('#filter_form').submit(function(event){
+		event.preventDefault();
+			$.get('/jobs.json', $(this).serialize(), function(jobs){
+				submitFilterForm(jobs);
+			});
+		var markerCluster = new MarkerClusterer(map, markers);
+	});	
+
+
+	$('#search_box').on('submit', function(event) {
+			event.preventDefault();
+			var geocoder = new google.maps.Geocoder();
+
+			geocoder.geocode( {'address': $('#searchTextField').val()}, function(results, status) {
+	      if (status == google.maps.GeocoderStatus.OK) {
+		        map.setCenter(results[0].geometry.location);
+						$.get('/jobs.json',$('#filter_form').serialize(), function(jobs) {
+							submitFilterForm(jobs);
+						});
+	      } else {
+	        alert("You search address was not recognised: " + status);
+	      }
+	    });
+  });
 
 
 
