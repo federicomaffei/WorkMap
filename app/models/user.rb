48b424b0b924e5dd517ffd62a894b0fe67f34233
federicomaffei <federico.maffei@gmail.com>
2014-07-29 00:15:25 +0100

    added s3 for images storing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :image, styles: { thumb: '300x300>' }
+  has_attached_file :image, styles: { thumb: '300x300>' }, s3_credentials: {
+      bucket: 'workmap',
+      access_key_id: Rails.application.secrets.s3_access_key,
+      secret_access_key: Rails.application.secrets.s3_secret_key
+    }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
