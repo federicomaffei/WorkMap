6762efd0433e46a9f8f0b00aaaca4f098b8b341f
Sasha Cooper <jinksy@gmail.com>
2014-07-29 14:57:46 +0100

    Introduced Google omniauth (clashes when you try to use it for an email address on the DB, eg FB omniauth)

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,8 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
         # user.name = auth.info.name   # assuming the user model has a name
-        # user.image = auth.info.image # assuming the user model has an image
+        url = auth.info.image
+        user.image = url.gsub('http:', "https:") # assuming the user model has an image
       end
     end
   end
