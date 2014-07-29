04a8912a50bcb9bd09456d7263f7f00437c30722
Sasha Cooper <jinksy@gmail.com>
2014-07-29 14:05:30 +0100

    omniauth working for google

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+  :recoverable, :rememberable, :trackable, :validatable,
+  :omniauthable, :omniauth_providers => [:facebook, :google]
 
   has_attached_file :image, styles: { thumb: '300x300>' }, s3_credentials: {
     bucket: 'workmap',
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
         # user.name = auth.info.name   # assuming the user model has a name
         # user.image = auth.info.image # assuming the user model has an image
+      end
     end
   end
-end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.omniauth_google_client_id, Rails.application.secrets.omniauth_google_client_secret
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, Rails.application.secrets.omniauth_google_client_id, Rails.application.secrets.omniauth_google_client_secret,
+#   {
+#       :name => "google",
+#       :scope => "email, profile, plus.me, http://gdata.youtube.com",
+#       :prompt => "select_account",
+#       :image_aspect_ratio => "square",
+#       :image_size => 50
+#     }
+# end
 
