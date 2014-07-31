1272752dda58eed5703c4e4fa29cd3c7883e8d44
Sasha Cooper <jinksy@gmail.com>
2014-07-31 12:05:50 +0100

    introduced mailer test

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,20 +1,8 @@
 class OrderMailer < ActionMailer::Base
-  default from: "no-reply@workmap.com"
+  default from: "from@example.com"
 
-  def confirmation(order)
-  	@order = order
-  	mail(to: order.employer.email, subject: "'#{order.job.advert_title}' placement confirmed")
+  def application(application)
+    mail(to: application.email, subject: "#{}")
   end
-
-  # def cancellation
-    
-  # end
-
-  # def status_change
-  # end
-
-  # def refunded
-  # end
-
-
+  
 end
