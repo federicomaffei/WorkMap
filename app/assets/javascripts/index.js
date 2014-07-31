8bb67358247b4057e11c58e9f332e2b189c420b2
Robin <robindoble@gmail.com>
2014-07-31 11:25:23 +0100

    refactoring indexjs

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-	// window.map works like var, but changes scope of following object to whole window, so you can access it from tests
+	
 	var latitude = (localStorage.getItem('lat')) ? localStorage.getItem('lat') : 51.497830;
 	var longitude = (localStorage.getItem('lng')) ? localStorage.getItem('lng') : -0.132523;
 
 
+	// window.map works like var, but changes scope of following object to whole window, so you can access it from tests
 	var mapOptions = {
 	  center: new google.maps.LatLng(latitude, longitude),
 	  zoom: 10,
@@ -28,8 +29,19 @@ $(document).ready(function(){
   //   ]
 	};
 
+	// initializing map object
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+	// additional map options
+	var zoom_options = { minZoom: 9, maxZoom: 17};
+	map.setOptions(zoom_options);
+
+	// define empty markers array, marketclusterer var and options
+	var markers = [];
+	var markerCluster;
+	var mcOptions = {gridSize: 80, maxZoom: 12};
+
+	// setting up google autocomplete for address search bar
 	var defaultBounds = new google.maps.LatLngBounds(
 		new google.maps.LatLng(51.2901, -0.5651),
 		new google.maps.LatLng(51.6167, 0.2463)
@@ -44,13 +56,51 @@ $(document).ready(function(){
 
 	autocomplete = new google.maps.places.Autocomplete(input, search_options);
 
-	var zoom_options = { minZoom: 9, maxZoom: 17};
-	map.setOptions(zoom_options);
 
-	var markers = [];
-	var markerCluster;
-	var mcOptions = {gridSize: 80, maxZoom: 12};
 
+
+
+	// on page reload render job markers and populate RHS job adverts pane
+		$.get("/jobs.json", function(jobs) {
+			// jobs.forEach(function(job) {
+				renderMarkers(job);
+			// });
+			updateAdvertColumn(jobs);
+			markerCluster = new MarkerClusterer(map, markers, mcOptions);
+		});
+
+
+
+	// refactored helper methods
+
+	// renders markers and infowindows on map, adds listener to each marker
+	var renderMarkers = function(jobs){
+		jobs.forEach(function(job) {		
+				var category = job.category;
+				var popup_template = $('#pop_up_job_advert').html();
+				var job_Info = Mustache.render(popup_template,job);	
+				var latLng = new google.maps.LatLng(job.latitude,job.longitude);
+				
+				var marker = new google.maps.Marker({
+					position: latLng,
+					title: job.advert_title,
+					category: job.category,
+					icon: "https://dl.dropboxusercontent.com/u/9315601/" + category + ".png",
+				});
+
+				marker.infowindow = new google.maps.InfoWindow( {
+					content: job_Info
+				});
+
+			  google.maps.event.addListener(marker, 'click', function() {
+			    marker.infowindow.open(map,marker);
+			  });
+
+				markers.push(marker);
+		});
+	};
+	
+	// calcs distance between a job marker can map center (in Kms)
 	var calcDistanceKms = function(map,job) {
 		var mapCenter = map.getCenter();
 		var markerLocation = new google.maps.LatLng(job.latitude,job.longitude);
@@ -58,6 +108,7 @@ $(document).ready(function(){
 	  return markerDistance/1000;
 	};
 
+	// clears all markers on map
 	var clearMarkers = function() {
 		for (var i = 0; i < markers.length; i++) {
 	    markers[i].setMap(null);
@@ -65,45 +116,48 @@ $(document).ready(function(){
 	  markers = [];
 	};
 
-	var renderMarker = function(job){
-		var category = job.category;
-		var popup_template = $('#pop_up_job_advert').html();
-		var job_Info = Mustache.render(popup_template,job);	
-		
-		var latLng = new google.maps.LatLng(job.latitude,job.longitude);
-		var marker = new google.maps.Marker({
-			position: latLng,
-			title: job.advert_title,
-			category: job.category,
-			icon: "https://dl.dropboxusercontent.com/u/9315601/" + category + ".png",
-		});
-
-		marker.infowindow = new google.maps.InfoWindow( {
-			content: job_Info
-		});
-
-	  google.maps.event.addListener(marker, 'click', function() {
-	    marker.infowindow.open(map,marker);
-	  });
-
-		markers.push(marker);
 
 
+	// filters out all job objects from jobs array that lie outside distance scope
+	var filterByMaxDistance = function(jobs,max_distance){
+		var filtereredJobs = [];
+			jobs.forEach(function(job) {
+				if (calcDistanceKms(map,job) <= job.max_distance) {	
+					filtereredJobs.push(job);
+				};
+			});
+		return filtereredJobs;
 	};
+
+	// sorts array of jobs by distance 
+	var sortByDistance = function(jobs){
+    	jobs.sort(function(a,b){
+			  if(a.distance > b.distance){ return 1}
+		    if(a.distance < b.distance){ return -1}
+	      return 0;
+			});
+			return jobs;
+	};
+
 
 	var submitFilterForm = function(jobs){
 		markers = []
 		var advertsRefined = [];
-			jobs.forEach(function(job) {
-				if (calcDistanceKms(map,job) <= job.max_distance) {			
-						renderMarker(job);
-						advertsRefined.push(job);
-					};
-			});
+		var max_distance = jobs[0].max_distance;
+
+		filterByMaxDistance(jobs,max_distance).forEach(function(job){
+				renderMarker(job);
+				advertsRefined.push(job);
+		});
+
 		updateAdvertColumn(advertsRefined);
 		markerCluster.clearMarkers()
 		markerCluster.addMarkers(markers);
 	};
+
+
+
+
 
 	var updateAdvertColumn = function(adverts){
 		$('.advert_column').empty();
@@ -113,15 +167,11 @@ $(document).ready(function(){
     $.each(adverts, function (i, job) {
 	      var template = $('#individual_job_advert').html();
 				job['linkid'] = i;
-				job['distance'] = calcDistanceKms(map,job).toFixed(2) +' Kms'; 
+				job['distance'] = Math.round(100*calcDistanceKms(map,job))/100; 
 				jobs.push(job);
     });
 
-    	jobs.sort(function(a,b){
-			   if(a.distance > b.distance){ return 1}
-			    if(a.distance < b.distance){ return -1}
-			      return 0;
-			});
+    var jobs = sortByDistance(jobs);
 
     $.each(jobs, function (i, job) {
       var template = $('#individual_job_advert').html();
@@ -141,28 +191,8 @@ $(document).ready(function(){
 
 	};
 
+
   
-	$.get("/jobs.json", function(jobs) {
-		jobs.forEach(function(job) {
-			renderMarker(job);
-		});
-		updateAdvertColumn(jobs);
-		markerCluster = new MarkerClusterer(map, markers, mcOptions);
-	});
-
-
-	$('#distance_slider').slider({
-		formater: function(value) {
-			return 'Current value: ' + value;
-		}
-	});
-
-
-	$('#wage_slider').slider({
-		formater: function(value) {
-			return 'Current value: ' + value;
-		}
-	});
 
 
 	var getFormData = function(){
@@ -212,6 +242,19 @@ $(document).ready(function(){
   
   });
 
+
+	$('#distance_slider').slider({
+		formater: function(value) {
+			return 'Current value: ' + value;
+		}
+	});
+
+
+	$('#wage_slider').slider({
+		formater: function(value) {
+			return 'Current value: ' + value;
+		}
+	});
 
 
 });
