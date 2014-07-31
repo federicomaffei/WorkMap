bb9eaaa0709e59bb534245239060b761eed4a583
Robin <robindoble@gmail.com>
2014-07-31 19:33:03 +0100

    added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,20 +29,19 @@ module ApplicationHelper
 		images[rand(images.size)]
 	end
 
-
-  # def bootstrap_class_for(flash_type)
-  #   case flash_type
-  #     when "success"
-  #       "alert-success"   # Green
-  #     when "error"
-  #       "alert-danger"    # Red
-  #     when "alert"
-  #       "alert-warning"   # Yellow
-  #     when "notice"
-  #       "alert-info"      # Blue
-  #     else
-  #       flash_type.to_s
-  #   end
-  # end
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success"
+        "alert-success"   # Green
+      when "error"
+        "alert-danger"    # Red
+      when "alert"
+        "alert-warning"   # Yellow
+      when "notice"
+        "alert-info"      # Blue
+      else
+        flash_type.to_s
+    end
+  end
 
 end
