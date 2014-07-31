a4e6a7ba20ed270b50bd975395aa52a305ba6319
Robin <robindoble@gmail.com>
2014-07-31 19:40:08 +0100

    custom failure devise included

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module WorkMap
   class Application < Rails::Application
 
     config.assets.initialize_on_precompile = false
+
+    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
