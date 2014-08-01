d1f874ff6a3a4a8fc108cf6e0c651f74f85e55c0
Robin <robindoble@gmail.com>
2014-08-01 01:37:12 +0100

    merging with master

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -166,7 +166,7 @@ $(document).ready(function(){
 	// define empty markers array, marketclusterer var and options
 	var markers = [];
 	var markerCluster;
-	var mcOptions = {gridSize: 80, maxZoom: 12};
+	var mcOptions = {gridSize: 100, maxZoom: 16};
 
 	// setting up google autocomplete for address search bar
 	var defaultBounds = new google.maps.LatLngBounds(
--- app/models/job.rb
+++ app/models/job.rb
@@ -9,8 +9,8 @@ class Job < ActiveRecord::Base
 	belongs_to :employer
 
 
-	geocoded_by :address
-	after_validation :geocode
+	# geocoded_by :address
+	# after_validation :geocode
 	after_destroy :send_cancellation_email
 
 	def send_cancellation_email
